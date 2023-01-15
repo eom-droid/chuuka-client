0620a261e77a841b0eb8823fa0a89dddd2a0de64
lunch0 <lunchboxno@gmail.com>
2023-01-15 20:40:53 +0900

    FEAT capacitor 설치

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.web.chuuka.client',
+  appName: 'Chuuka',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
