90378b2b835c5ad123a01ee6931dac425294fe2f
lunch0 <lunchboxno@gmail.com>
2023-02-06 00:28:17 +0900

    FEAT 일정/소식

---
+++ src/components/news/NewsCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="x-basic-padding text-left font-normal">
+    <div class="flex justify-between pt-2.5">
+      <span class="text-base">{{ props.title }}</span>
+    </div>
+    <a
+      :href="props.content"
+      class="text-sm mt-2.5 text-url-blue underline"
+      target="_blank"
+      >{{ props.content }}</a
+    >
+    <div
+      class="rounded-md border border-mid-gray box-border px-3 py-4 mt-2.5 grid grid-cols-6"
+    >
+      <div class="col-span-1 minWidth">
+        <div class="image_wrapper">
+          <img :src="props.ogImage" class="image_wrapper_child" />
+        </div>
+      </div>
+      <div class="text-sm w-full col-span-5">
+        <p class="break-words ml-2.5 og-title">{{ props.ogTitle }}</p>
+        <p class="text-xs break-words ml-2.5 mt-2 og-description">
+          {{ props.ogDescription }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps([
+  "title",
+  "content",
+  "ogImage",
+  "ogTitle",
+  "ogDescription",
+  "state",
+]);
+</script>
+
+<style scoped>
+.image_wrapper {
+  position: relative;
+  padding-top: 100%;
+  width: 100%;
+  height: 0px;
+}
+
+.image_wrapper > .image_wrapper_child {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.minWidth {
+  min-width: 50px;
+}
+.og-title {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+}
+.og-description {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+</style>
--- src/views/store/news/News.vue
+++ src/views/store/news/News.vue
@@ -1,96 +1,12 @@
-<template>
-  <div class="text-left my-3 whitespace-pre-line grid gap-3 text-sm">
-    <img
-      src="@/assets/gif/loadingIcon.gif"
-      v-if="isLoading"
-      class="w-40 mx-auto"
-    />
-
-    <div
-      v-for="(news, index) in getNews"
-      :key="news.id"
-      class="border border-mid-gray shadow-sm rounded-md px-3 pb-3 pt-2"
-      @click="onClickNews(news)"
-      v-else
-    >
-      <div class="text-xs">
-        {{ getKoreanDateTime(news.modDtime.toDate()) }}
-      </div>
-      <div class="mt-1 my-3"><hr /></div>
-      <div>
-        <div class="text-base font-semibold">{{ news.title }}</div>
-        <div
-          v-if="
-            news.photos != undefined &&
-            news.photos.length === 1 &&
-            news.photos[0].link != ''
-          "
-        >
-          <div class="w-full border border-mid-gray mt-3">
-            <img
-              :src="news.photos[0].link"
-              class="w-full h-20v object-contain"
-            />
-          </div>
-        </div>
-        <div v-if="news.photos != undefined && news.photos.length > 1">
-          <div class="flex w-full mt-3 h-20v">
-            <div class="w-2/3 border border-mid-gray h-full">
-              <img
-                :src="news.photos[0].link"
-                class="object-contain h-full w-full"
-              />
-            </div>
-
-            <div class="w-1/3 ml-1 flex flex-col justify-between">
-              <div class="mb-1 w-full border border-mid-gray h-1/2">
-                <img
-                  :src="news.photos[1].link"
-                  class="object-contain w-full h-full"
-                />
-              </div>
-
-              <div
-                class="w-full font-medium h-1/2 bg-usual-black opacity-70 text-white text-2xl flex relative"
-              >
-                <img
-                  :src="news.photos[2].link"
-                  class="object-contain w-full h-full opacity-40 absolute"
-                  v-if="news.photos.length >= 3"
-                />
-                <span class="m-auto"> +{{ news.photos.length - 2 }} </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="text-sm mt-3 tracking-tighter leading-tight">
-          {{ cutLine(news.content) }}
-          <br />
-          <button class="text-text-gray" v-show="countLine(news.content) >= 2">
-            ...더보기
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div
-      class="mt-5 text-neutral-500 text-center text-base"
-      v-if="getNews.length <= 0"
-    >
-      일정/소식이 없습니다.
-    </div>
-  </div>
-</template>
 <script setup lang="ts">
 import { getAllNews, INews } from "@/api/m1/news";
 import { useNewsStore } from "@/stores/news";
 import { useStoreInfoStore } from "@/stores/storeInfo";
-import { getKoreanDateTime } from "@/utils/moment";
 import { storeToRefs } from "pinia";
 import { ref, onMounted, onUnmounted, onUpdated } from "vue";
 import { throttle } from "lodash";
 import { router } from "@/router/router";
-import { connectStorageEmulator } from "@firebase/storage";
+import NewsCard from "@/components/news/NewsCard.vue";
 
 const pinia = useStoreInfoStore();
 const piniaNews = useNewsStore();
@@ -99,13 +15,15 @@ const { getStoreInfo } = storeToRefs(pinia);
 const { setInitStoreId, setNews, setSelectedNews, setSavedTop } = piniaNews;
 const isLoading = ref(true);
 const listnerFunc = throttle(scrollFunc, 300);
+const newsArray = ref([] as INews[]);
 
-onMounted(() => {
+onMounted(async () => {
   document
     .getElementById("mainWrapper")
     ?.addEventListener("scroll", listnerFunc);
   window.addEventListener("scroll", listnerFunc);
   init();
+  newsArray.value = await getAllNews(getStoreInfo.value.id);
 });
 
 onUpdated(() => {
@@ -175,3 +93,34 @@ function cutLine(newLine: string) {
   }
 }
 </script>
+
+<template>
+  <div class="text-left my-3 whitespace-pre-line grid gap-3 text-sm">
+    <img
+      src="@/assets/gif/loadingIcon.gif"
+      v-if="isLoading"
+      class="w-40 mx-auto"
+    />
+
+    <div v-for="ele in newsArray" class="cursor-pointer">
+      <div class="border-t border-mid-gray w-full bg-light-gray h-1.5"></div>
+      <div class="mt-4">
+        <span class="ml-4">{{ ele.modDtime.toDate().toLocaleString() }}</span>
+        <NewsCard
+          :title="ele.title"
+          :content="ele.content"
+          :og-image="ele.openGraph.ogImage"
+          :og-title="ele.openGraph.ogTitle"
+          :ogDescription="ele.openGraph.ogDescription"
+        />
+      </div>
+    </div>
+
+    <div
+      class="mt-5 text-neutral-500 text-center text-base"
+      v-if="getNews.length <= 0"
+    >
+      일정/소식이 없습니다.
+    </div>
+  </div>
+</template>
